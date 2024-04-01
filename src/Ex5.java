3c5471c3bf6d55707db67d7f98110ec895355efc
Juliana Stadler <julianastadlerberger@gmail.com>
2024-04-01 19:02:56 -0300

    Ex5

---
+++ src/Ex5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        // 5. A Loja Mamão com Açúcar está vendendo seus produtos em 5 (cinco) prestações sem juros.
+        // Faça um programa que receba um valor de uma compra e mostre o valor das prestações.
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor total da compra: ");
+        float val_compra = scanner.nextFloat();
+
+        Ex5_Compra compra = new Ex5_Compra(val_compra);
+
+        System.out.println("Cada prestação terá um valor de: R$"+ compra.prestacao());
+
+        scanner.close();
+    }
+    
+}
---
+++ src/Ex5_Compra.java
@@ -0,0 +1,12 @@
+
+public class Ex5_Compra {
+    private float val_compra;
+
+    public Ex5_Compra (float val_compra){
+        this.val_compra = val_compra;
+    }
+    public float prestacao(){
+        return val_compra/5;
+    }
+
+}
